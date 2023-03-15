a176fa2dfafd89faf329f2dc6b4dbabd3be367ea
O. Yuanying <yuanying@fraction.jp>
2023-03-15 11:55:44 +0000

    Split shared code to model package

--- model/model.go
+++ model/model.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"sync"
+
+	"github.com/ieee0824/gopenai-api/api"
+	"github.com/yuanying/myao/utils"
+	"k8s.io/klog/v2"
+)
+
 type Opts struct {
 	OpenAIAccessToken    string
 	OpenAIOrganizationID string
@@ -13,4 +21,131 @@ type Model interface {
 	Reply(content string) (string, error)
 	Summarize()
 	Name() string
+}
+
+type memory struct {
+	api.Message
+	summary bool
+}
+
+type Shared struct {
+	Name        string
+	SystemText  string
+	SummaryText string
+	Temperature float32
+	ErrorText   string
+	OpenAI      api.OpenAIAPIIface
+
+	// mu protects memories from concurrent access.
+	mu       sync.RWMutex
+	memories []memory
+}
+
+func (s *Shared) Remember(summary bool, role, content string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	klog.Infof("memories: %v", len(s.memories))
+	s.memories = append(s.memories, memory{
+		summary: summary,
+		Message: api.Message{
+			Role:    role,
+			Content: content,
+		},
+	})
+}
+
+func (s *Shared) Forget(num int) {
+	klog.Infof("Try forget the old memries")
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if num > len(s.memories) {
+		num = len(s.memories)
+	}
+	s.memories = s.memories[num:]
+}
+
+func (s *Shared) Messages() []api.Message {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	rtn := make([]api.Message, len(s.memories)+1)
+	rtn[0] = api.Message{Role: "system", Content: s.SystemText}
+
+	for i := range s.memories {
+		rtn[i+1] = s.memories[i].Message
+	}
+	return rtn
+}
+
+func (s *Shared) Summarize() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.needsSummary() {
+		memories := make([]memory, len(s.memories))
+		copy(memories, s.memories)
+		klog.Infof("Needs summary")
+		go func() {
+			s.Reply(true, "system", s.SummaryText)
+		}()
+	}
+}
+
+func (s *Shared) needsSummary() bool {
+	if len(s.memories) > 15 {
+		for _, mem := range s.memories[len(s.memories)-15:] {
+			if mem.summary == true {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func (s *Shared) Reply(summary bool, role, content string) (string, error) {
+	klog.Infof("Requesting chat completions...: %v", content)
+	temperature := s.Temperature
+	if summary {
+		temperature = 0
+	}
+	messages := s.Messages()
+	if content != "" {
+		messages = append(messages, api.Message{Role: role, Content: content})
+	}
+
+	output, err := s.OpenAI.ChatCompletionsV1(&api.ChatCompletionsV1Input{
+		Model:       utils.ToPtr("gpt-3.5-turbo"),
+		Messages:    messages,
+		Temperature: utils.ToPtr(temperature),
+	})
+	if output.Usage != nil {
+		klog.Infof("Usage: prompt %v tokens, completions %v tokens", output.Usage.PromptTokens, output.Usage.CompletionTokens)
+		if output.Usage.PromptTokens > 3584 {
+			s.Forget(5)
+		} else if output.Usage.PromptTokens > 3328 {
+			s.Forget(4)
+		} else if output.Usage.PromptTokens > 3072 {
+			s.Forget(3)
+		}
+	}
+	if err != nil {
+		if output.Error != nil {
+			klog.Errorf("OpenAI returns error: %v\n, message: %v", err, output.Error.Message)
+			if output.Error.Code == "context_length_exceeded" {
+				s.Forget(8)
+			}
+		}
+		return s.ErrorText, err
+	}
+
+	reply := output.Choices[0].Message
+	if summary != true && content != "" {
+		s.Remember(summary, role, content)
+	}
+	s.Remember(summary, reply.Role, reply.Content)
+	if summary {
+		klog.Infof("Summary: %v", reply.Content)
+	}
+
+	return reply.Content, nil
 }
--- model/myao/model.go
+++ model/myao/model.go
@@ -4,7 +4,6 @@ import (
 	_ "embed"
 	"fmt"
 	"strings"
-	"sync"
 
 	"github.com/ieee0824/gopenai-api/api"
 	"github.com/ieee0824/gopenai-api/config"
@@ -17,21 +16,9 @@ import (
 
 var _ model.Model = (*Myao)(nil)
 
-type Memory struct {
-	api.Message
-	summary bool
-}
-
 type Myao struct {
-	name   string
+	model  *model.Shared
 	Config *configs.Config
-	openAI api.OpenAIAPIIface
-
-	// mu protects memories from concurrent access.
-	mu       sync.RWMutex
-	memories []Memory
-
-	systemText string
 }
 
 func New(opts *model.Opts) (*Myao, error) {
@@ -53,20 +40,25 @@ func New(opts *model.Opts) (*Myao, error) {
 	systemText := fmt.Sprintf(config.SystemText, sb.String())
 
 	m := &Myao{
-		name:       config.Name,
-		openAI:     openAI,
-		Config:     config,
-		systemText: systemText,
+		model: &model.Shared{
+			Name:        config.Name,
+			OpenAI:      openAI,
+			SummaryText: config.SummaryText,
+			Temperature: config.Temperature,
+			SystemText:  systemText,
+			ErrorText:   config.ErrorText,
+		},
+		Config: config,
 	}
 	for _, msg := range config.InitConversations {
-		m.remember(false, msg.Role, msg.Content)
+		m.model.Remember(false, msg.Role, msg.Content)
 	}
 
 	return m, nil
 }
 
 func (m *Myao) Name() string {
-	return m.name
+	return m.model.Name
 }
 
 func (m *Myao) FormatText(user, content string) string {
@@ -74,126 +66,13 @@ func (m *Myao) FormatText(user, content string) string {
 }
 
 func (m *Myao) Remember(role, content string) {
-	m.remember(false, role, content)
-}
-
-func (m *Myao) remember(summary bool, role, content string) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	klog.Infof("Memories: %v", len(m.memories))
-	m.memories = append(m.memories, Memory{
-		summary: summary,
-		Message: api.Message{
-			Role:    role,
-			Content: content,
-		},
-	})
+	m.model.Remember(false, role, content)
 }
 
 func (m *Myao) Summarize() {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	if m.needsSummary() {
-		memories := make([]Memory, len(m.memories))
-		copy(memories, m.memories)
-		klog.Infof("Needs summary")
-		go func() {
-			m.reply(true, "system", m.Config.SummaryText)
-		}()
-	}
-}
-
-func (m *Myao) needsSummary() bool {
-	if len(m.memories) > 15 {
-		for _, mem := range m.memories[len(m.memories)-15:] {
-			if mem.summary == true {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
-
-func (m *Myao) Memories() []Memory {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-	systemText := m.systemText
-	if len(m.memories) < 15 {
-		systemText = systemText + m.Config.InitText
-	}
-	system := Memory{Message: api.Message{Role: "system", Content: systemText}}
-	return append([]Memory{system}, m.memories...)
-}
-
-func toMessages(ms []Memory) []api.Message {
-	rtn := make([]api.Message, len(ms))
-
-	for i := range ms {
-		rtn[i] = ms[i].Message
-	}
-	return rtn
+	m.model.Summarize()
 }
 
 func (m *Myao) Reply(content string) (string, error) {
-	return m.reply(false, "user", content)
-}
-
-func (m *Myao) reply(summary bool, role, content string) (string, error) {
-	klog.Infof("Requesting chat completions...: %v", content)
-	temperature := m.Config.Temperature
-	if summary {
-		temperature = 0
-	}
-	messages := toMessages(m.Memories())
-	if content != "" {
-		messages = append(messages, api.Message{Role: role, Content: content})
-	}
-
-	output, err := m.openAI.ChatCompletionsV1(&api.ChatCompletionsV1Input{
-		Model:       utils.ToPtr("gpt-3.5-turbo"),
-		Messages:    messages,
-		Temperature: utils.ToPtr(temperature),
-	})
-	if output.Usage != nil {
-		klog.Infof("Usage: prompt %v tokens, completions %v tokens", output.Usage.PromptTokens, output.Usage.CompletionTokens)
-		if output.Usage.PromptTokens > 3584 {
-			m.forget(5)
-		} else if output.Usage.PromptTokens > 3328 {
-			m.forget(4)
-		} else if output.Usage.PromptTokens > 3072 {
-			m.forget(3)
-		}
-	}
-	if err != nil {
-		if output.Error != nil {
-			klog.Errorf("OpenAI returns error: %v\n, message: %v", err, output.Error.Message)
-			if output.Error.Code == "context_length_exceeded" {
-				m.forget(8)
-			}
-		}
-		return m.Config.ErrorText, err
-	}
-
-	reply := output.Choices[0].Message
-	if summary != true && content != "" {
-		m.remember(summary, role, content)
-	}
-	m.remember(summary, reply.Role, reply.Content)
-	if summary {
-		klog.Infof("Summary: %v", reply.Content)
-	}
-
-	return reply.Content, nil
-}
-
-func (m *Myao) forget(num int) {
-	klog.Infof("Try forget the old memries")
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if num > len(m.memories) {
-		num = len(m.memories)
-	}
-	m.memories = m.memories[num:]
+	return m.model.Reply(false, "user", content)
 }
